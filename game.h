64c6622e9bd936d4673ca77877750dc62a2af67b
jamie_chen <jamiechen989@hotmail.com>
2018-07-19 16:10:34 -0400

    updated game.h

--- game.h
+++ game.h
@@ -1,1 +1,26 @@
-a
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include "vector"
+#include "textdisplay.h"
+#include 'all races'
+
+class Game {
+  vector<vector<tile*>> emptyMap;
+  vector<vector<tile*>> map;
+  Tile* PC;
+  Floor level;
+
+public:
+  Game(char pc);
+  ~Game();
+  init();
+  refreshMap(); //check if win in this method
+  spawnAt(int x, int y);
+  movePC(int x, int y);
+  moveEnemies();
+  PCAttach(int x, int y);
+  usePotion(int x, int y);
+
+};
+#endif
