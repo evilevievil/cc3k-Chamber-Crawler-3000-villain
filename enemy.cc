813f6555d088451c15e76a6c24294e8b28e0ee31
jamie_chen <jamiechen989@hotmail.com>
2018-07-22 17:35:03 -0400

    updated enemy

--- enemy.cc
+++ enemy.cc
@@ -6,6 +6,30 @@ Enemy::Enemy(char c, Posn p, Tile* t, int maxhp, int hp, int atk, int def):
 
 Enemy::~Enemy() {}
 
+void Enemy::attack(Tile* t){
+  t.beAttacked(this);
+}
+
+void Enemy::beAttacked(Enemy& pc){
+  hp -= damage(pc.getAtk(), def);
+  if(hp <= 0) dropReward();
+}
+
+void Enemy::beAttacked(Vampire& pc){
+  hp -= damage(pc.getAtk(), def);
+  pc.restoreHp();
+  if(hp <= 0) dropReward();
+}
+
+void Enemy::beAttacked(Goblin& pc){
+  hp -= damage(pc.getAtk(), def);
+  if(hp <= 0){
+    pc.addGold(5);
+    dropReward();
+  }
+}
+
+
 void Enemy::checkSurroundings(){
 }
 
--- enemy.h
+++ enemy.h
@@ -5,16 +5,15 @@
 
 class Enemy: public Race{	
 public:
-	void attack(Tile* t);
-	void beAttacked(Shade& pc);
-	void beAttacked(Drow& pc);
-	void beAttacked(Vampire& pc);
-	void beAttacked(Troll& pc);
-	void beAttacked(Goblin& pc);
-	void randMove(Map& map);
+	virtual void dropReward() = 0;
+	virtual void attack(Tile* t);
+	virtual void beAttacked(Enemy& pc);
+	virtual void beAttacked(Vampire& pc);
+	virtual void beAttacked(Goblin& pc);
+	virtual void randMove(Map& map);
 	void checkSurroundings();	
 	Enemy(char c, Posn p, Tile* t, int maxhp, int hp, int atk, int def);
-	virtual ~Enemy();
+	virtual ~Enemy() = 0;
 };
 
 #endif
