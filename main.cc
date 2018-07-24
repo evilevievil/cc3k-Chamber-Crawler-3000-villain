abe19acf08097c83cd1b272e48fd8394610cfe23
jxtu2 <jxtu@uwaterloo.ca>
2018-07-24 16:42:22 -0400

    updated

--- main.cc
+++ main.cc
@@ -1,13 +1,28 @@
 #include <string>
-#include <ostream>
+#include <iostream>
+#include "game.h"
 using namespace std;
 
 int main (){
 	string welcome= "Hello Welcome to ChamberCrawler3000!";
-		try{
-
-	}catch(...){
-		
+	string choosePc= "Please choose your hero charcter! (s , d, v, g, t)";
+	string mapname= "map.txt";
+	cout<<welcome<<endl;
+	cout<<choosePC<<endl;
+	char c;
+	while (cin<<c){
+		if (c=='s'){
+			Game(c, mapname);
+		}else if (c== 'd'){
+			Game (c, mapname);
+		}else if (c=='v'){
+			Game (c, mapname);
+		}else if (c=='g'){
+			Game (c == 'd');
+		}else if (c == 'g'){
+			Game (c, mapname);
+		}else if (c==t){
+			Game (c, mapname);
+		}
 	}
-	
 }
