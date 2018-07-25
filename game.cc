634b8607e82d6e587af5c41c352b6c428ac6709a
Eva Feng <evahf1231@gmail.com>
2018-07-24 23:52:13 -0400

    game generator completed

--- game.cc
+++ game.cc
@@ -121,13 +121,151 @@ void Game::usePotion(Posn p){
   } else {throw "Invalid use potion";}
 }
 
+Posn generatorpos() {
+  int num, x, y, h, w, s, n, m;
+  srand(time(NULL));
+  do{
+  num = rand() % 5;
+  s = rand() % 2;
+  switch(num) {
+    case 0: 
+      x = 3; 
+      y = 3;
+      h = 4;
+      w = 26;
+      break;     
+    case 1:
+      x = 38; 
+      y = 10;
+      h = 3;
+      w = 13;
+      break;
+    case 2:
+      x = 3;
+      y = 15;
+      h = 7;
+      w = 21;
+      break;
+    case 3:
+      if(s) {
+        x = 39;
+        y = 3;
+        h = 4;
+        w = 34;
+      } else {
+        x = 61;
+        y = 7;
+        h = 6;
+        w = 15;
+      }
+      break;
+    case 4:
+      if(s) {
+        x = 65;
+        y = 16;
+        h = 3;
+        w = 11;
+      } else {
+        x = 37;
+        y = 19;
+        h = 3;
+        w = 39;
+      }      
+      break;
+  }
+    n = rand() % w;
+    m = rand() % h;
+  } while (!map[x+n][y=m]->isWalkable)
+  Posn p{x+n, y+m};
+  return p;
+}
 
-void Game::generatorStair() {}
-void Game::generatorEnemy(){}
+void Game::generatorStair() {
+  Posn &p = generatorpos();
+  int px = p.first;
+  int py = p.second;
+  Tile *newtile = new Tile{'\', true};
+  swap(newtile, map[x[y]]);
+  delete newtile;
+}
 
-void Game::generatorGold(){}
+void Game::generatorEnemy(){
+  srand(time(NULL));
+  int num = rand() % 18 + 1;
+  Posn &p = generatorpos();
+  int px = p.first;
+  int py = p.second;
+  if (1<=num && num<=4) {
+    Tile* newtile = new Human{}; 
+  } else if (5<=num && num<=7) {
+    Tile* newtile = new Dwarf{};
+  } else if (8<=num && num<=12) {
+    Tile* newtile = new Halfling{};
+  } else if (13<=num && num<=14) {
+    Tile* newtile = new Elf{}; 
+  } else if (15<=num && num<=16) {
+    Tile* newtile = new Orc{};
+  } else if (17<=num && num<=18) {
+    Tile *newtile = new Merchant{};
+  } 
+  swap(newtile, map[x[y]]);
+  delete newtile;
+}
 
-void Game::generatorPotion(){}
+void Game::generatorGold(){
+  srand(time(NULL));
+  int num = rand() % 8 + 1;
+  Posn &p = generatorpos();
+  int px = p.first;
+  int py = p.second;
+  if (1<=num && num<=5) {
+    Tile* newtile = new NormalHoard{}; 
+  } else if (6<=num && num<=7) {
+    Tile* newtile = new SmallHoard{};
+  } else if (num==8) {
+    int pn = px-1;
+    int pm = py-1;
+    for (;pn<px+2;++pn) {
+      for (;pm<py+2;++pm) {
+        if (map[pn][pm]->isWalkable) {break;}
+      }
+      if (map[pn][pm]->isWalkable) {break;}
+    }
+    Tile* newtile = new DragonHoard{}; 
+    Tile* dragontile = new Dragon{};
+    swap(newtile, map[px][py]);
+    swap(dragontile, map[pn][pm]);
+    delete dragontile;
+    delete newtile; 
+    return;
+  } 
+  swap(newtile, map[x[y]]);
+  delete newtile;
+}
+
+void Game::generatorPotion(){
+
+  srand(time(NULL));
+  int num = rand() % 6 + 1;
+  Posn &p = generatorpos();
+  int px = p.first;
+  int py = p.second;
+  if (1==num) {
+    Tile* newtile = new RH{}; 
+  } else if (2==num) {
+    Tile* newtile = new BA{};
+  } else if (3==num) {
+    Tile* newtile = new BD{};
+  } else if (4==num) {
+    Tile* newtile = new PH{}; 
+  } else if (5==num) {
+    Tile* newtile = new WA{};
+  } else if (6==num) {
+    Tile *newtile = new WD{};
+  } 
+  swap(newtile, map[x[y]]);
+  delete newtile;
+}
 
 
 
